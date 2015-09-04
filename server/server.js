cf0d9371ae4d1c3b10facf67c32aeb9991773b3b
Sean Nakamura <seanenakamura@gmail.com>
2015-09-03 23:45:41 -1000

    use static public folder directory

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/../public'));
+
 var server = app.listen(8080, function (){
   var HOST = server.address().address;
   var PORT = server.address().port;
