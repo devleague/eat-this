cd9737c340d1ad7beeb948f74db76e4c6e2b0c28
Sean Nakamura <seanenakamura@gmail.com>
2015-09-05 15:06:16 -1000

    ui.router implemented for state pages

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,23 @@
 (function (){
-  var app = angular.module('eatApp', []);
+  var app = angular.module('eatApp', ['ui.router']);
+
+  app.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider
+      .otherwise('/');
+    $stateProvider
+      .state('home', {
+        url: '/',
+      })
+      .state('fetch', {
+        url: '/fetch',
+        templateUrl: '../templates/go-fetch.html',
+        controller: 'goFetchController'
+      })
+      .state('results', {
+        url: '/results',
+        templateUrl: '../templates/results.html',
+        controller: 'resultsController'
+      });
+  }]);
 
 })();
