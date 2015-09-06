3385e12fe0fd792ea9ad7265c4171e6607265b9b
Sean Nakamura <seanenakamura@gmail.com>
2015-09-05 19:10:53 -1000

    append mock json data to dom

--- app/js/controllers/final-result.controller.js
+++ app/js/controllers/final-result.controller.js
@@ -3,11 +3,13 @@
     .controller('resultsController', [
         '$scope',
         'eatTitle',
+        'VenueService',
         finalResults
       ]);
 
-  function finalResults ($scope, eatTitle) {
+  function finalResults ($scope, eatTitle, VenueService) {
     $scope.title = eatTitle;
     $scope.byline = 'This is the final results';
+    $scope.returnVenue = VenueService.getVenues();
   }
 })();
