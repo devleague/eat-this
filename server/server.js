88b1c0d2dfa6840dd42d6d85963911550556c07c
oteswang <oteswang@shackmbp.local>
2015-09-08 23:51:55 -1000

    latitude and longitude not being passed to server

--- server/server.js
+++ server/server.js
@@ -3,17 +3,28 @@ var app = express();
 
 var yelp = require("yelp").createClient({
   consumer_key: process.env.YELP_CONSUMER_KEY,
-  consumer_secret: process.env.YELP_CONSUER_SECRET,
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
   token: process.env.YELP_TOKEN,
-  token_secret: process.env.YELP_TOKEN_SECRET
+  token_secret: process.env.YELP_TOKEN_SECRET,
+  ssl: true
 });
 
 app.use(express.static(__dirname + '/../app'));
 
-app.get('/index/v2/business', function (req, res) {
+app.get('/locations', function (req, res) {
+
+  var latitude = req.body.latitude;
+  var longitude = req.body.longitude;
+  var location = latitude + ',' + longitude;
+
+  console.log(location);
+
   yelp.search({term: "food", location: "Manoa", radius_filter: 3220}, function (error, data) {
-    console.log(error);
-    console.log(data);
+
+  // yelp.search({ term: "food", ll: location, radius_filter: 3220 }, function (error, data) {
+    // console.log(error);
+    // console.log(data);
+    console.log(location);
 
     var yelpData = {
       name: data.businesses[0].name
@@ -22,6 +33,10 @@ app.get('/index/v2/business', function (req, res) {
     res.json(yelpData);
   });
 
+});
+
+app.post('/locations', function (req, res) {
+  console.log(req.body.latitude);
 });
 
 var server = app.listen(8080, function (){
