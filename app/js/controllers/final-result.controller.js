b2f067f529557018b2b5f6322b0f3074986bc89b
Sean Nakamura <seanenakamura@gmail.com>
2015-09-10 20:20:08 -1000

    added image scope for final result controller

--- app/js/controllers/final-result.controller.js
+++ app/js/controllers/final-result.controller.js
@@ -16,6 +16,7 @@
         $scope.rating = result.rating;
         $scope.name = result.name;
         $scope.display_phone = result.display_phone;
+        $scope.image = result.image_url;
       });
   }
 })();
