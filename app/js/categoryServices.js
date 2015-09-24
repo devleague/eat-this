6f0071c77eb30dd4c14289b908e26d46cf220c17
Paul Keahi <paulkeahi@gmail.com>
2015-09-23 17:24:13 -1000

    adding Services for Category

--- app/js/categoryServices.js
+++ app/js/categoryServices.js
@@ -3,9 +3,13 @@
     .module('eatApp')
     .service('CategoryService', ['$resource' , function ($resource){
 
-      this.getCategory = function (QQ){
+      // this.getCategory = function (QQ){
 
-      };
+      //   var catOptions =
+
+      //   var Category = $resources('/api/restaurants', catOptions);
+      //   return Category.query().$promise;
+      // };
 
       this.displayCategory = function (cat){
 
