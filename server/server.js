ba013e972b36c587b2de9f278a7aca1979f31df8
Sean Nakamura <seanenakamura@gmail.com>
2015-09-23 20:43:51 -1000

    added CORS bypass

--- server/server.js
+++ server/server.js
@@ -17,6 +17,12 @@ var yelp = require("yelp").createClient({
 app.use(express.static(__dirname + '/../app'));
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/venues/:id', function (req, res) {
   yelp.business(req.params.id, function (error, data) {
     var venue = data;
