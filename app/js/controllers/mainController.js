a7ab1163da56c71b84d872c120e980d32c4f2d39
Julia Bond <julia@yogaed.com>
2015-09-24 18:54:17 -1000

    Adding autocomplete function.

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -5,16 +5,30 @@
         '$scope',
         'eatTitle',
         'Geolocator',
+        'uiGmapGoogleMapApi',
          mainController
       ]);
 
-  function mainController ($rootScope, $scope, eatTitle, geolocation) {
+  function mainController ($rootScope, $scope, eatTitle, geolocation, googleMaps) {
     $scope.title = eatTitle;
 
-    $rootScope.userLocation
-        .then(function (position){
-          console.log(position.coords);
-          $scope.location = position.coords.latitude;
-        });
+    geolocation()
+      .then(function (position){
+        $scope.position = position;
+        $scope.location = position.coords.latitude;
+      }, function(reason){
+        $scope.message = reason.message;
+
+        googleMaps
+          .then(function(maps){
+            var placeSearch, autocomplete;
+            console.log(maps);
+
+            function initAutocomplete(){
+              autocomplete = new maps.places.Autocomplete((document.getElementById('autocomplete')), {types: ['geocode']});
+              autocomplete.addListener('place_changed', fillInAddress);
+            }
+          });
+      });
   }
 })();
