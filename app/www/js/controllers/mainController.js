2fb0408f70cbf0af9c1273723d5eacabb89e002e
Sean Nakamura <seanenakamura@gmail.com>
2015-10-01 19:59:47 -1000

    added open modal emit

--- app/www/js/controllers/mainController.js
+++ app/www/js/controllers/mainController.js
@@ -4,14 +4,13 @@
         '$rootScope',
         '$scope',
         '$state',
-        '$ionicModal',
         'eatTitle',
         'Geolocator',
         'uiGmapGoogleMapApi',
          mainController
       ]);
 
-  function mainController ($rootScope, $scope, $state, $ionicModal, eatTitle, geolocation, googleMaps) {
+  function mainController ($rootScope, $scope, $state, eatTitle, geolocation, googleMaps) {
     $scope.title = eatTitle;
 
     $scope.place = null;
@@ -23,17 +22,7 @@
       })
       .catch(function(error){
         console.log(error);
-        $scope.modal.show();
-
-      });
-
-    $ionicModal
-      .fromTemplateUrl('templates/set-location.html', {
-        scope: $scope,
-        anmiation: 'slide-in-up'
-      })
-      .then(function (modal){
-        $scope.modal = modal;
+        $rootScope.$emit('openModal');
       });
   }
 })();
