ed00ce0e4f0d111906cb8df3eddafcea9195dd65
Julia Bond <julia@yogaed.com>
2015-10-07 21:51:20 -1000

    Working on popup for server error.

--- app/www/js/controllers/mainController.js
+++ app/www/js/controllers/mainController.js
@@ -4,43 +4,82 @@
         '$rootScope',
         '$scope',
         '$state',
+        '$ionicPopup',
+        'VenueService',
         'Geolocator',
         'uiGmapGoogleMapApi',
          mainController
       ]);
 
-  function mainController ($rootScope, $scope, $state, geolocation, googleMaps) {
+  function mainController ($rootScope, $scope, $state, $ionicPopup, VenueService, geolocation, googleMaps) {
 
     $scope.place = null;
 
-    $scope.openModal = function (){
-      $rootScope.$emit('openModal');
-    };
-
     $rootScope.userLocation
-      .then(function (position){
-        $scope.position = position;
-
-        googleMaps
-        .then(function(maps){
-          var geocoder = new maps.Geocoder();
-          var latlng = {lat: parseFloat(position.coords.latitude), lng: parseFloat(position.coords.longitude)};
-          geocoder.geocode({'location': latlng}, function(results, status) {
-            if (status === google.maps.GeocoderStatus.OK) {
-              if (results[1]) {
-                $rootScope.userLocation.country = results[results.length - 1].formatted_address;
-              } else {
-                window.alert('No results found');
-              }
-            } else {
-              window.alert('Geocoder failed due to: ' + status);
-            }
-          });
-        });
+      .then(function(position){
+        loadVenues(position);
       })
       .catch(function(error){
         console.log(error);
         $rootScope.$emit('openModal');
       });
+
+    $scope.openModal = function (){
+      $rootScope.$emit('openModal');
+    };
+
+    function loadVenues(position) {
+      $scope.position = true;
+      VenueService
+        .getVenues(position.coords.latitude, position.coords.longitude)
+        .then(function(venues){
+          $scope.showAlert = function (){
+            var myPopup = $ionicPopup.alert({
+              title: '<b>UH OH!</b>',
+              template: 'Our app does not work in your location yet.',
+              // scope: $scope,
+              // buttons:[{
+              //   text: 'Give Up Now' },
+              //   {
+              //   text: 'Set Another Location',
+              //   type: 'button-positive',
+              //   onTap: function() {
+              //     myPopup.close();
+              //     return $rootScope.$emit('openModal');
+              //   }
+              // }]
+            });
+            myPopup.then(function(res){
+              if(res){
+                console.log('You clicked Set Location!');
+              } else {
+                console.log('You gave up');
+              }
+            });
+          };
+          $scope.showAlert();
+
+          // if(venues){
+          //   googleMaps
+          //   .then(function(maps){
+          //     var geocoder = new maps.Geocoder();
+          //     var latlng = {lat: parseFloat(position.coords.latitude), lng: parseFloat(position.coords.longitude)};
+          //     geocoder.geocode({'location': latlng}, function(results, status) {
+          //       if (status === google.maps.GeocoderStatus.OK) {
+          //         if (results[1]) {
+          //           $rootScope.userLocation.country = results[results.length - 1].formatted_address;
+          //         } else {
+          //           window.alert('No results found');
+          //         }
+          //       } else {
+          //         window.alert('Geocoder failed due to: ' + status);
+          //       }
+          //     });
+          //   });
+          // } else {
+
+          // }
+        });
+    }
   }
 })();
